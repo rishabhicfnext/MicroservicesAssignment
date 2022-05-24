77bc1d0deb2037f397d215f7b257a043db886363
rishabhicfnext <rishabh.nigam@icfnext.com>
2022-05-24 17:02:15 +0530

    swagger enable in user microservice

--- user-service/src/main/java/com/user/controller/UserController.java
+++ user-service/src/main/java/com/user/controller/UserController.java
@@ -24,66 +24,87 @@ public class UserController {
 
     @PostMapping("/createuser")
     public ResponseEntity<Object> createUser(@Valid @RequestBody User user) {
+        log.info("Inside create user method in controller !!");
         int isExist = userService.isUserExistByOfficeID(user.getOfficeID());
         if (isExist != 0) {
+            log.error("User already exist !!");
             throw new UserAlreadyExistException();
         } else {
             user = userService.createUser(user);
+            log.info("User created successfully !!");
             return new ResponseEntity<Object>(user, HttpStatus.CREATED);
         }
     }
 
     @PutMapping("/updateuser/{userID}")
     public ResponseEntity<Object> updateUser(@PathVariable("userID") long userID, @Valid @RequestBody User user) {
+        log.info("Inside update user method in controller !!");
         boolean isExist = userService.isUserExist(userID);
         if (isExist) {
+            log.info("User exist with this id");
             user.setUserID(userID);
             userService.updateUser(user);
+            log.info("User Successfully updated with id : " + userID);
             return new ResponseEntity<Object>("User Updated successfully : \n" + user, HttpStatus.OK);
         } else {
+            log.error("User Not found with this id : " + userID);
             throw new UserNotFoundException();
         }
     }
 
     @DeleteMapping("/deleteuser/{userID}")
     public ResponseEntity<Object> deleteUser(@PathVariable("userID") long userID) {
+        log.info("Inside delete user method controller !!");
         boolean isExist = userService.isUserExist(userID);
         if (isExist) {
+            log.info("User exist with this id");
             User user = userService.getUser(userID);
             userService.deleteUser(userID);
+            log.info("User successfully deleted with id : " + userID);
             return new ResponseEntity<Object>(user, HttpStatus.GONE);
         } else {
+            log.error("User not found with this id : " + userID);
             throw new UserNotFoundException();
         }
     }
 
     @GetMapping("/getuserbyid/{userID}")
     public ResponseEntity<Object> getUserById(@PathVariable("userID") long userID) {
+        log.info("Inside get user by id method in controller !!");
         boolean isExist = userService.isUserExist(userID);
         if (isExist) {
+            log.info("User exist with this id");
             User user = userService.getUser(userID);
+            log.info("User getting successfully with id : " + userID);
             return new ResponseEntity<Object>(user, HttpStatus.OK);
         } else {
+            log.error("User not found with this id : " + userID);
             throw new UserNotFoundException();
         }
     }
 
     @GetMapping("/getallusers")
     public ResponseEntity<Object> getAllUsers() {
+        log.info("Inside get all user method in controller !!");
         List<User> userList = userService.getUsers();
         if (userList.isEmpty()) {
+            log.error("User list is empty");
             throw new UserNotFoundException();
         } else {
+            log.info("Getting all user list");
             return new ResponseEntity<Object>(userList, HttpStatus.OK);
         }
     }
 
     @GetMapping("/getallusersbyprojectid/{projectID}")
     public ResponseEntity<Object> getAllUsersByProjectID(@PathVariable("projectID") long projectID) {
+        log.info("Inside getallusersbyprojectid method in controller !!");
         List<User> userList = userService.getUsersByProjectID(projectID);
         if (userList.isEmpty()) {
+            log.error("user not found with this project id : " + projectID);
             throw new UserNotFoundException();
         } else {
+            log.info("User list is geting with this project id : " + projectID);
             return new ResponseEntity<Object>(userList, HttpStatus.OK);
         }
     }
--- user-service/src/main/java/com/user/service/impl/UserServiceImpl.java
+++ user-service/src/main/java/com/user/service/impl/UserServiceImpl.java
@@ -5,6 +5,7 @@ import com.user.repository.UserRepository;
 import com.user.service.UserService;
 import com.user.vo.Project;
 import com.user.vo.ResponseTemplateVO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -14,6 +15,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
+@Slf4j
 public class UserServiceImpl implements UserService {
 
     @Autowired
@@ -27,32 +29,41 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public ResponseTemplateVO getUserWithProject(long userID) {
+        log.info("Inside getUserWithProject method in service");
         ResponseTemplateVO responseTemplateVO = new ResponseTemplateVO();
         User user = userRepository.findById(userID).get();
+        log.info("User find by this id : " + userID);
         Project project = restTemplate.getForObject("http://project-service/project/getbyprojectid/" + user.getProjectID(), Project.class);
+        log.info("Calling project-service microservice using RestTemplate !!");
+        log.info("Project get with this user id : " + userID);
         responseTemplateVO.setUser(user);
         responseTemplateVO.setProject(project);
+        log.info("User and Project successfully get and returned");
         return responseTemplateVO;
     }
 
     @Override
     public User createUser(@Valid User user) {
+        log.info("Inside create user method in service !!");
         //user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 
     @Override
     public void updateUser(@Valid User user) {
+        log.info("Inside update user method in service");
         userRepository.saveAndFlush(user);
     }
 
     @Override
     public void deleteUser(long userID) {
+        log.info("Inside delete user method in service");
         userRepository.deleteById(userID);
     }
 
     @Override
     public User getUser(long userID) {
+        log.info("Inside get user method in service");
         Optional<User> op = userRepository.findById(userID);
         User e = op.get();
         return e;
@@ -60,23 +71,28 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public List<User> getUsers() {
+        log.info("Inside getUsers method in service");
         List<User> allUsers = userRepository.findAll();
         return allUsers;
     }
 
     @Override
     public List<User> getUsersByProjectID(long projectID) {
+        log.info("Inside getuserbyprojectid method in service !!");
         List<User> allUsersBySameProjectID = userRepository.allUsersBySameProjectID(projectID);
+        log.info("List of user getting and returning");
         return allUsersBySameProjectID;
     }
 
     @Override
     public boolean isUserExist(long userID) {
+        log.info("Inside isUserExist method in service");
         return userRepository.existsById(userID);
     }
 
     @Override
     public int isUserExistByOfficeID(@Valid String officeID) {
+        log.info("Inside isUserExistByOfficeID method in service !!");
         return userRepository.existsByOfficeID(officeID);
     }
 }
