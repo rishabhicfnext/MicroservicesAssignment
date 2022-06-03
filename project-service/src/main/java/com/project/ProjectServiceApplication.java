caf2f4e506c66998faccfe0ad4be72d47ba4581b
rishabhicfnext <rishabh.nigam@icfnext.com>
2022-06-03 14:20:44 +0530

    API-Gateway completed

--- project-service/src/main/java/com/project/ProjectServiceApplication.java
+++ project-service/src/main/java/com/project/ProjectServiceApplication.java
@@ -7,11 +7,13 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @Slf4j
+@EnableEurekaClient
 public class ProjectServiceApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
